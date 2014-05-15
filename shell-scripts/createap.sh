89a71c887c61286fd2314288810439e4f5658304
Holyshatots <themcaffee@gmail.com>
2014-05-14 19:58:38 -0600

    Added some random shell scripts

---
+++ shell-scripts/createap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+airmon-ng start wlan1
+echo 'Started wlan1'
+nohup airbase-ng -e RainbowDash -q mon0
+echo 'Started access point'
+sleep 3
+ifconfig at0 up
+echo 'Brought at0 up'
+ifconfig at0 192.168.1.1 netmask 255.255.255.0
+echo 'Assigned at0 ip 192.168.1.1'
+#Kill off dnsmasq
+killall dnsmasq
+service dnsmasq start
---
+++ shell-scripts/createhiddenap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+airmon-ng start wlan1
+echo 'Started wlan1'
+nohup airbase-ng -X -q mon0
+echo 'Started access point'
+sleep 3
+ifconfig at0 up
+echo 'Brought at0 up'
+ifconfig at0 192.168.1.1 netmask 255.255.255.0
+echo 'Assigned at0 ip 192.168.1.1'
+
