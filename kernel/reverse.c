75fff7b6013bae614062e269a3de88d8cdf1c927
Holyshatots <themcaffee@gmail.com>
2014-05-30 10:47:48 -0600

    Added kernel playing around

---
+++ kernel/reverse.c
@@ -0,0 +1,15 @@
+/*
+Mitch McAffee
+* 
+http://www.linuxvoice.com/be-a-kernel-hacker/?pk_campaign=hn&pk_kwd=3
+* 
+*/
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mitchell McAffee <mitch.mcaffee@gmail.com>");
+MODULE_DESCRIPTION("In-kernel phrase reverser");
+
